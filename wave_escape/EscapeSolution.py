b35c7b3b783443cc09abad8c3c80881992e21c5a
Cesar B. Rocha <crocha@ucsd.edu>
2017-11-02 15:10:14 -0700

    Adds plot for notes

--- wave_escape/EscapeSolution.py
+++ wave_escape/EscapeSolution.py
@@ -73,15 +73,58 @@ tmax = 6
 fig = plt.figure(figsize=(6.5,6.5))
 ax = fig.add_subplot(aspect=1)
 
-for t in np.arange(0,6,.025):
+plot_figs2movie = False
 
-    plt.clf()
+if plot_figs2movie:
+    for t in np.arange(0,6,.025):
+
+        plt.clf()
+        plt.contour(x/R,y/R,psi,cp,colors='k')
+        plt.contourf(x/R,y/R,phi(t).real,cu,cmap=cmocean.cm.balance)
+        plt.xlabel(r"$x/R$")
+        plt.ylabel(r"$y/R$")
+        plt.title(r"$t\,\alpha = %3.2f$" %(t*alpha))
+        plt.draw()
+        tit = "%3.2f" %(t*alpha)
+        plt.savefig("figs/"+tit+".png")
+        plt.pause(0.01)
+
+
+plot_fig2notes = True
+
+if plot_fig2notes:
+
+    cu = np.hstack([np.arange(-6,0,1), np.arange(1,7,1)])
+
+    fig = plt.figure(figsize=(8.5,4.5))
+    ax = fig.add_subplot(141,aspect=1)
     plt.contour(x/R,y/R,psi,cp,colors='k')
-    plt.contourf(x/R,y/R,phi(t).real,cu,cmap=cmocean.cm.balance)
+    plt.contourf(x/R,y/R,phi(0).real,cu,cmap=cmocean.cm.balance,extend="both")
     plt.xlabel(r"$x/R$")
     plt.ylabel(r"$y/R$")
-    plt.title(r"$t\,\alpha = %3.2f$" %(t*alpha))
-    plt.draw()
-    tit = "%3.2f" %(t*alpha)
-    plt.savefig("figs/"+tit+".png")
-    plt.pause(0.01)
+    plt.title(r"$t\,\alpha = %3.2f$" %(0*alpha))
+
+    ax = fig.add_subplot(142,aspect=1)
+    plt.contour(x/R,y/R,psi,cp,colors='k')
+    plt.contourf(x/R,y/R,phi(2).real,cu,cmap=cmocean.cm.balance,extend="both")
+    plt.xlabel(r"$x/R$")
+    plt.yticks([])
+    plt.title(r"$t\,\alpha = %3.2f$" %(2*alpha))
+
+
+    ax = fig.add_subplot(143,aspect=1)
+    plt.contour(x/R,y/R,psi,cp,colors='k')
+    plt.contourf(x/R,y/R,phi(3).real,cu,cmap=cmocean.cm.balance,extend="both")
+    plt.xlabel(r"$x/R$")
+    plt.yticks([])
+    plt.title(r"$t\,\alpha = %3.2f$" %(3*alpha))
+
+    ax = fig.add_subplot(144,aspect=1)
+    plt.contour(x/R,y/R,psi,cp,colors='k')
+    plt.contourf(x/R,y/R,phi(5).real,cu,cmap=cmocean.cm.balance,extend="both")
+    plt.xlabel(r"$x/R$")
+    plt.yticks([])
+    #plt.ylabel(r"$y/R$")
+    plt.title(r"$t\,\alpha = %3.2f$" %(5*alpha))
+
+    plt.savefig("figs/WaveEscape.png")
