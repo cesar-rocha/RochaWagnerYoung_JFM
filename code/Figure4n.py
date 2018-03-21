2739ace2960bc4d2b249f57d352f4584f945d31a
Cesar B. Rocha <crocha@ucsd.edu>
2018-03-20 17:29:37 -0700

    Update formats

--- code/Figure4n.py
+++ code/Figure4n.py
@@ -1,160 +1,136 @@
-
 """
-    Plots figure 5: snapshots vorticity and near-inertial
-                    kinetic energy density of reference turbulence simulation.
+    Plots figure 2: energy time series, budgets,
+                    wave-vorticity correlation
+                    of the Lamb-Chapygin dipole solution.
 """
 
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import cmocean
 
 from Utils import *
 
 plt.close('all')
 
-#pathi = "outputs/decaying_turbulence/reference/coupled/"
-pathi = "outputs/high_res/decaying_turbulence/parameter_exploration/Uw0.1/lambdaz397.5/"
+pathi = "outputs/lambdipole/"
 patho = "../writeup/figs/"
 
-## get params
 params = h5py.File(pathi+"parameters.h5","r")
+diags = h5py.File(pathi+"diagnostics.h5")
+
+## get params
 Ue, ke = params['dimensional/Ue'][()], params['dimensional/ke'][()]
 Te = params['dimensional/Te'][()]
 Uw = params['dimensional/Uw'][()]
-m = params['dimensional/m'][()]
-N0 = params['dimensional/N'][()]
-f0 = params['dimensional/f0'][()]
-lam2 = (N0/f0/m)**2
-## get grid
-setup = h5py.File(pathi+"setup.h5","r")
-x, y = setup['grid/x'][:]*ke/(2*np.pi), setup['grid/y'][:]*ke/(2*np.pi)
-#x -= x.mean()
-#y -= y.mean()
-k, l = setup['grid/k'][:], setup['grid/l'][:]
-k, l = np.meshgrid(k,l)
+
+## get diagnostics
+time = diags['time'][:]
+KE_qg = diags['ke_qg'][:]
+PE_niw = diags['pe_niw'][:]
+KE_niw = diags['ke_niw'][:]
+ENS_qg = diags['ens'][:]
+g1 = diags['gamma_r'][:]
+g2 = diags['gamma_a'][:]
+pi = diags['pi'][:]
+cKE_niw = diags['cke_niw'][:]
+iKE_niw = diags['ike_niw'][:]
+ep_phi = diags['ep_phi'][:]
+ep_psi = diags['ep_psi'][:]
+chi_q =  diags['chi_q'][:]
+chi_phi =  diags['chi_phi'][:]
+conc_niw =  diags['conc_niw'][:]
+skew =  diags['skew'][:]
+
+## calculate tendency from energy time series
+dt = time[1]-time[0]
+dPE = np.gradient(PE_niw,dt)
+dKE = np.gradient(KE_qg,dt)
+diKE_niw = np.gradient(iKE_niw,dt)
+
+res_ke = dKE-(-g1-g2+ep_psi)
+res_pe = dPE-g1-g2-chi_phi
+
+## plotting
+fig = plt.figure(figsize=(8.5,4))
+lw, alp = 2.,1.
+KE0 = KE_qg[0]
+tmax = time[-1]
+
+ax = fig.add_subplot(121)
+fig.subplots_adjust(wspace=.55)
+plt.plot([-5,35],[0,0],'k-',linewidth=0.85)
+
+plt.plot(time/Te,(KE_niw-KE_niw[0])/KE_niw[0],label=r"$\Delta\langle\mathcal{A}\rangle/\langle\mathcal{A}\rangle(0)$",linewidth=lw,alpha=alp)
+plt.plot(time/Te,(KE_qg-KE_qg[0])/KE0,label=r"$\Delta\langle\mathcal{K}\rangle/\langle\mathcal{K}\rangle(0)$",linewidth=lw,alpha=alp)
+plt.plot(time/Te,(PE_niw-PE_niw[0])/KE0,label=r'$\Delta\langle\mathcal{P}\rangle/\langle\mathcal{K}\rangle(0)$',linewidth=lw,alpha=alp)
+
+plt.plot(time/Te,(PE_niw-PE_niw[0]+KE_qg-KE_qg[0])/KE0,'--',
+        label=r'$(\Delta\langle\mathcal{P}\rangle+\Delta\langle\mathcal{K}\rangle)/\langle\mathcal{K}\rangle(0)$',
+        linewidth=lw,alpha=alp)
+
+plt.text(10,.43,r"$\Delta\langle\mathcal{P}\rangle$")
+plt.text(10,-.45,r"$\Delta\langle\mathcal{K}\rangle$")
+plt.text(15,0.025,r"$\Delta\langle\mathcal{A}\rangle$")
+
+plt.xlim(0,30)
 
 
-#files = ['000000000016667.h5', '000000001333333.h5',
-#            '000000002666667.h5', '000000008000000.h5']
+plt.ylim(-0.6,0.6)
+plt.ylabel(r'Energy change about $t=0$')
+#plt.legend(loc=(0.05,0.9))
+#plt.plot([0,tmax/Te],[0]*2,'-',linewidth=1,color="0.5")
+plot_fig_label(ax, label="a",xc=0.05,yc = 0.05)
+plt.xlabel(r"Time [$t \times U_e k_e$]")
+plt.xticks([0,10,20,30])
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax = fig.add_subplot(122)
+fig.subplots_adjust(wspace=.45)
+plt.plot([-5,35],[0,0],'k-',linewidth=0.85)
 
-files = ['000000001000000.h5', '000000004000000.h5', '000000020000000.h5']
-files = ['000000001000000.h5', '000000008000000.h5', '000000040000000.h5']
-files = ['000000000500000.h5', '000000004000000.h5', '000000010000000.h5']
+plt.plot(time/Te,Te*g1/KE0,label=r'$\Gamma_r$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*g2/KE0,label=r'$\Gamma_a$',linewidth=lw,alpha=alp)
+#plt.plot(time/Te,Te*chi_phi/KE0,label=r'$\varepsilon_\mathcal{P}$',linewidth=lw,alpha=alp)
+#plt.plot(time/Te,Te*(g1+g2+chi_phi)/KE0,label=r'$\Gamma_r+\Gamma_a+ \varepsilon_\mathcal{P}$',
+ #                       linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*dPE/KE0,label=r'$\mathrm{d}\langle\mathcal{P}\rangle/\mathrm{d}t$',linewidth=lw,alpha=alp)
+#plt.legend(loc=(0.5,0.75),ncol=1)
+plt.xlabel(r"Time [$t \times U_e k_e$]")
+plt.ylim(-0.02,0.08)
+plt.ylabel(r'Power $[\dot P \times {2 k_e^{-1}}/{U_e}^{-3} ]$')
+plot_fig_label(ax, label="b",xc=0.05,yc = 0.05)
+plt.xticks([0,10,20,30])
+plt.text(13.25,-0.0135,r"$\Gamma_r$")
+plt.text(14.5,0.025,r"$\Gamma_a$")
+#plt.text(4.25,0.06,r"$\Gamma_r+\Gamma_a-\varepsilon_{\mathcal{P}}$")
+plt.text(3.5,0.06,r"$\partial_t \langle \mathcal{P} \rangle$")
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
 
-def plot_snapshot(fig, snap, panel = 1, frac = 2):
-    """ Plot snapshot of vorticity and
-            near-inertial kinetic energy density """
+plt.xlim(0,30)
 
-    ticks = np.arange(0,xlim[1],2)
+# ax = fig.add_subplot(223)
+# plt.plot(time/Te,Te*pi/KE0,label=r'$\Pi$',linewidth=lw,alpha=alp)
+# plt.plot(time/Te,Te*ep_psi/KE0,label=r'$\epsilon_\phi$',linewidth=lw,alpha=alp)
+# plt.plot(time/Te,Te*(pi+ep_phi)/KE0,label=r'$\Pi+\epsilon_\phi$',linewidth=lw,alpha=alp)
+# plt.plot(time/Te,Te*diKE_niw/KE0,'k--',label=r'$\dot K_w^i$'
+#                 ,linewidth=lw,alpha=alp)
+# plt.xlabel(r"Time [$t \times U_e k_e$]")
+# plt.ylabel(r'Power $[\dot E \times {2 k_e}/{U_e} ]$')
+# plt.legend(loc=1,ncol=2)
+# plot_fig_label(ax, label="c")
+#
+# fig.subplots_adjust(hspace=.125)
+#
+# ax = fig.add_subplot(133)
+# p1 = ax.plot(time/Te,conc_niw,linewidth=lw,alpha=alp,label='NIW concentration, $C$')
+# plt.ylabel(r"Wave-vorticity correlation [r]")
+# plt.xlabel(r"Time [$t \times U_e k_e$]")
+# plt.plot([0,tmax/Te],[0]*2,'--',color="0.5")
+# plt.ylim(-0.8,0.8)
+# plot_fig_label(ax, label="c")
+# plt.xticks([0,10,20,30])
 
-
-
-    t = snap['t'][()]/Te
-    q = snap['q'][:]/(Ue*ke)
-    uw, vw, ww, pw, bw = wave_fields(snap['phi'][:],f0,lam2,snap['t'][()],k,l,m)
-
-    b = bw/(Uw*m*ke*f0*lam2)
-    phi2 = np.abs(snap['phi'])**2/Uw**2
-
-    
-    nx,ny = q.shape
-    nmax = nx//2
-
-    ax2 = fig.add_subplot(3,3,panel,aspect=1)
-    fig.subplots_adjust(wspace=.01)
-    fig.subplots_adjust(hspace=.155)
-    
-    pc_q = ax2.contourf(x[:nmax,:nmax],y[:nmax,:nmax], q[:nmax,:nmax],
-                            cq, vmin=cq.min(), vmax=cq.max(),
-                            cmap = cmocean.cm.curl, extend='both')
-    ax2.set_xlim(xlim[0],xlim[1])
-    ax2.set_ylim(xlim[0],xlim[1])
-
-
-
-    ax = fig.add_subplot(3,3,panel+3,aspect=1)
-   
-    pc_phi = ax.contourf(x[:nmax,:nmax],y[:nmax,:nmax], phi2[:nmax,:nmax],
-                            cphi,vmin=cphi.min(),vmax=cphi.max(),extend='max',
-                            cmap = cmocean.cm.ice_r)
-    ax.set_xlim(xlim[0],xlim[1])
-    ax.set_ylim(xlim[0],xlim[1])
-
-
-    ax3 = fig.add_subplot(3,3,panel+6,aspect=1)
-    pc_b = ax3.contourf(x[:nmax,:nmax],y[:nmax,:nmax], b[:nmax,:nmax],
-                            cb, vmin=cb.min(), vmax=cb.max(),
-                            cmap = cmocean.cm.balance, extend='both')
-    ax3.set_xlim(xlim[0],xlim[1])
-    ax3.set_ylim(xlim[0],xlim[1])
-
-    if panel == 1:
-        ax.set_xticks([])
-        ax2.set_xticks([])
-        ax2.set_yticks(ticks)
-        ax.set_yticks(ticks)
-        ax3.set_yticks(ticks)
-        ax3.set_xticks(ticks)
-        ax2.set_ylabel(r"$y\times k_e/2\pi$")
-        ax3.set_ylabel(r"$y\times k_e/2\pi$")
-        ax3.set_xlabel(r"$x\times k_e/2\pi$")
-        ax.set_ylabel(r"$y\times k_e/2\pi$")
-        fig.subplots_adjust(wspace=.165)
-        fig.subplots_adjust(hspace=.165)
-    elif panel == 2 or panel == 3:
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax2.set_xticks([])
-        ax2.set_yticks([])
-        ax2.set_xticks([])
-        ax2.set_yticks([])
-        ax3.set_yticks([])
-        ax3.set_xticks(ticks)
-        ax3.set_xlabel(r"$x\times k_e/2\pi$")
-    else:
-        pass
-
-    plot_fig_label(ax2, xc =0.7, yc=1.05, label= r"$t \times U_e k_e$ = "+
-                    str(int(round(t))), facecolor="1.0",boxstyle=None,alpha=0.)
-
-    return pc_phi, pc_q, pc_b
-
-cphi = np.arange(0.,5.,0.1)
-cq   = np.arange(-2.5,2.6,.1)
-cb   = np.arange(-2.5,2.6,.1)
-
-xlim = [0,5]
-fig = plt.figure(figsize=(8.5,7.25))
-
-panel = 1
-for fni in files:
-    snap = h5py.File(pathi+"snapshots/"+fni)
-    imphi, imq,imb = plot_snapshot(fig, snap, panel)
-    panel += 1
-
-# colorbar
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([0.81, .387, 0.013,0.22])
-fig.colorbar(imphi, cax=cbar_ax,label=r"Wave action density" "\n" r"$[\mathcal{A} \times 2 f_0 /U_w^2]$",
-                orientation='vertical',ticks=np.arange(0,7,1))
-
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([.81, .65, 0.013, 0.22])
-fig.colorbar(imq, cax=cbar_ax,label=r"PV $[q/(U_e k_e)]$",
-                orientation='vertical',ticks=np.arange(-5,5,1))
-
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([.81, .1155, 0.013, 0.22])
-fig.colorbar(imb, cax=cbar_ax,label=r"Wave buoyancy $[b/B]$",
-                orientation='vertical',ticks=np.arange(-5,5,1))
-
-plt.savefig(patho+"fig3.png", transparent=True,
-            pad_inches=0,
-            bbox_inches='tight',dpi=300)
-
-plt.savefig(patho+"fig4.tiff", transparent=True,
-            pad_inches=0,
-            bbox_inches='tight',dpi=300)
-
+plt.savefig(patho+"fig2.pdf", bbox_inches='tight',pad_inces=0, )
+plt.savefig(patho+"fig4.tiff", bbox_inches='tight',pad_inces=0, )
 
---
+++ code/Figure5n.py
@@ -0,0 +1,160 @@
+
+"""
+    Plots figure 5: snapshots vorticity and near-inertial
+                    kinetic energy density of reference turbulence simulation.
+"""
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import cmocean
+
+from Utils import *
+
+plt.close('all')
+
+#pathi = "outputs/decaying_turbulence/reference/coupled/"
+pathi = "outputs/high_res/decaying_turbulence/parameter_exploration/Uw0.1/lambdaz397.5/"
+patho = "../writeup/figs/"
+
+## get params
+params = h5py.File(pathi+"parameters.h5","r")
+Ue, ke = params['dimensional/Ue'][()], params['dimensional/ke'][()]
+Te = params['dimensional/Te'][()]
+Uw = params['dimensional/Uw'][()]
+m = params['dimensional/m'][()]
+N0 = params['dimensional/N'][()]
+f0 = params['dimensional/f0'][()]
+lam2 = (N0/f0/m)**2
+## get grid
+setup = h5py.File(pathi+"setup.h5","r")
+x, y = setup['grid/x'][:]*ke/(2*np.pi), setup['grid/y'][:]*ke/(2*np.pi)
+#x -= x.mean()
+#y -= y.mean()
+k, l = setup['grid/k'][:], setup['grid/l'][:]
+k, l = np.meshgrid(k,l)
+
+
+#files = ['000000000016667.h5', '000000001333333.h5',
+#            '000000002666667.h5', '000000008000000.h5']
+
+files = ['000000001000000.h5', '000000004000000.h5', '000000020000000.h5']
+files = ['000000001000000.h5', '000000008000000.h5', '000000040000000.h5']
+files = ['000000000500000.h5', '000000004000000.h5', '000000010000000.h5']
+
+def plot_snapshot(fig, snap, panel = 1, frac = 2):
+    """ Plot snapshot of vorticity and
+            near-inertial kinetic energy density """
+
+    ticks = np.arange(0,xlim[1],2)
+
+
+
+    t = snap['t'][()]/Te
+    q = snap['q'][:]/(Ue*ke)
+    uw, vw, ww, pw, bw = wave_fields(snap['phi'][:],f0,lam2,snap['t'][()],k,l,m)
+
+    b = bw/(Uw*m*ke*f0*lam2)
+    phi2 = np.abs(snap['phi'])**2/Uw**2
+
+    
+    nx,ny = q.shape
+    nmax = nx//2
+
+    ax2 = fig.add_subplot(3,3,panel,aspect=1)
+    fig.subplots_adjust(wspace=.01)
+    fig.subplots_adjust(hspace=.155)
+    
+    pc_q = ax2.contourf(x[:nmax,:nmax],y[:nmax,:nmax], q[:nmax,:nmax],
+                            cq, vmin=cq.min(), vmax=cq.max(),
+                            cmap = cmocean.cm.curl, extend='both')
+    ax2.set_xlim(xlim[0],xlim[1])
+    ax2.set_ylim(xlim[0],xlim[1])
+
+
+
+    ax = fig.add_subplot(3,3,panel+3,aspect=1)
+   
+    pc_phi = ax.contourf(x[:nmax,:nmax],y[:nmax,:nmax], phi2[:nmax,:nmax],
+                            cphi,vmin=cphi.min(),vmax=cphi.max(),extend='max',
+                            cmap = cmocean.cm.ice_r)
+    ax.set_xlim(xlim[0],xlim[1])
+    ax.set_ylim(xlim[0],xlim[1])
+
+
+    ax3 = fig.add_subplot(3,3,panel+6,aspect=1)
+    pc_b = ax3.contourf(x[:nmax,:nmax],y[:nmax,:nmax], b[:nmax,:nmax],
+                            cb, vmin=cb.min(), vmax=cb.max(),
+                            cmap = cmocean.cm.balance, extend='both')
+    ax3.set_xlim(xlim[0],xlim[1])
+    ax3.set_ylim(xlim[0],xlim[1])
+
+    if panel == 1:
+        ax.set_xticks([])
+        ax2.set_xticks([])
+        ax2.set_yticks(ticks)
+        ax.set_yticks(ticks)
+        ax3.set_yticks(ticks)
+        ax3.set_xticks(ticks)
+        ax2.set_ylabel(r"$y\times k_e/2\pi$")
+        ax3.set_ylabel(r"$y\times k_e/2\pi$")
+        ax3.set_xlabel(r"$x\times k_e/2\pi$")
+        ax.set_ylabel(r"$y\times k_e/2\pi$")
+        fig.subplots_adjust(wspace=.165)
+        fig.subplots_adjust(hspace=.165)
+    elif panel == 2 or panel == 3:
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax2.set_xticks([])
+        ax2.set_yticks([])
+        ax2.set_xticks([])
+        ax2.set_yticks([])
+        ax3.set_yticks([])
+        ax3.set_xticks(ticks)
+        ax3.set_xlabel(r"$x\times k_e/2\pi$")
+    else:
+        pass
+
+    plot_fig_label(ax2, xc =0.7, yc=1.05, label= r"$t \times U_e k_e$ = "+
+                    str(int(round(t))), facecolor="1.0",boxstyle=None,alpha=0.)
+
+    return pc_phi, pc_q, pc_b
+
+cphi = np.arange(0.,5.,0.1)
+cq   = np.arange(-2.5,2.6,.1)
+cb   = np.arange(-2.5,2.6,.1)
+
+xlim = [0,5]
+fig = plt.figure(figsize=(8.5,7.25))
+
+panel = 1
+for fni in files:
+    snap = h5py.File(pathi+"snapshots/"+fni)
+    imphi, imq,imb = plot_snapshot(fig, snap, panel)
+    panel += 1
+
+# colorbar
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.81, .387, 0.013,0.22])
+fig.colorbar(imphi, cax=cbar_ax,label=r"Wave action density" "\n" r"$[\mathcal{A} \times 2 f_0 /U_w^2]$",
+                orientation='vertical',ticks=np.arange(0,7,1))
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([.81, .65, 0.013, 0.22])
+fig.colorbar(imq, cax=cbar_ax,label=r"PV $[q/(U_e k_e)]$",
+                orientation='vertical',ticks=np.arange(-5,5,1))
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([.81, .1155, 0.013, 0.22])
+fig.colorbar(imb, cax=cbar_ax,label=r"Wave buoyancy $[b/B]$",
+                orientation='vertical',ticks=np.arange(-5,5,1))
+
+plt.savefig(patho+"fig3.png", transparent=True,
+            pad_inches=0,
+            bbox_inches='tight',dpi=300)
+
+plt.savefig(patho+"fig5.tiff", transparent=True,
+            pad_inches=0,
+            bbox_inches='tight',dpi=300)
+
+
